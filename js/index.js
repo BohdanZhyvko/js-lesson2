cbd51a923d00c82581efaab4b464638226c20c8d
Bohdan <bohdan.zhyvko@gmail.com>
2017-02-25 22:18:11 +0000

    add the sequence of Fibonacci numbers, recursion and cycle

--- js/index.js
+++ js/index.js
@@ -1,59 +1,55 @@
+var numFibonacci = Number(prompt('Введіть N-не значення ряду Фібоначчі: '));
 
-//for (var i = 100; i >= 1 ; i-2) {
-//    if( !(i % 2) ){
-//        document.writeln(i +'<br>')
-//    }
-//}
+var summa = 0;
+var firstFib = 0;
+var secondFib = 1;
 
-//var cats = ['Pushok', 'Tuzik', 'Barsik'];
-//    cats[15] = 'Marchik'; 
 
-//for (var i = 0; i < cats.length ; i++) {
-//        document.writeln(cats[i] +'<br>')
-//}
+document.writeln('N = ' + numFibonacci + '<br>');
+summa = cyclicFibonacci(numFibonacci, firstFib, secondFib, summa)
+document.writeln('<br>Сума чисел: ' + summa);
+document.writeln('<br>Рекурсивний спосіб: ');
+summa = recursionFibonacci(numFibonacci, firstFib, secondFib, 0, 0, 1)
+document.writeln('<br>Сума чисел: ' + summa);
 
-var cats = {
-    young: "Pushok",
-    old: "Sirko"
-}
-
-cats['angry'] = 'Murko';
-
-document.writeln( cats['angry'] +'<br>');
-
-for( var key in cats){
-    document.writeln(cats[key]+'<br>');
-}
-
-var cat = {
-    name: "Pushok",
-    color: "black",
-    run: function(){
-        document.writeln("Cat " + this.name +
-        " has run! <br>")
+//The sequence of Fibonacci numbers, cyclic 
+function cyclicFibonacci(numFibonacci, fib1, fib2, sum) {
+    document.writeln('Циклічний спосіб: ');
+    for (var i = 1, num = 0; i < numFibonacci; i++) {
+        if (i == 1) {
+            document.writeln(fib1 + ' ' + fib2 + ' ');
+            sum = fib1 + fib2;
+        }
+        num = fib1 + fib2;
+        if (i % 2) {
+            fib1 = num;
+        } else {
+            fib2 = num;
+        }
+        document.writeln(num);
+        sum += num;
     }
+    return sum;
 }
 
-function Cat( name, color ){
-    this.name = name;
-    this.color = color;
-    this.run = function(){
-        document.writeln("Cat " + this.name +
-        " has run! <br>")
+//The sequence of Fibonacci numbers, recursion 
+function recursionFibonacci(numFib, fib1, fib2, sum, num, counter) {
+    if (numFib > counter) {
+         if (counter == 1) {
+            document.writeln(fib1 + ' ' + fib2 + ' ');
+            sum = fib1 + fib2;
+        }
+        num = fib1 + fib2;
+        if (counter % 2) {
+            fib1 = num;
+        } else {
+            fib2 = num;
+        }
+        document.writeln(num);
+        sum += num;
+        counter++;
+        return recursionFibonacci(numFib, fib1, fib2, sum, num, counter);
+    } else {
+        return sum;
     }
-    return this;
-}
-
-
-
-var silvester = new Cat("Silvester"," gray");
-cat.run();
-silvester.name;
-silvester.color;
-document.writeln(silvester.name + ' ' +silvester.color);
-silvester.run();
-
-for(var i = 0, k = 0; i <= 5; i++){
-    k += i; 
-    document.writeln(k);
 }
